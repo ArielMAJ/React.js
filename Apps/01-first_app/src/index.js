39698e12c2d32eebc865798239243264dfa14376
ArielMAJ <ariel.maj@hotmail.com>
2023-02-14 13:12:45 -0300

    [UPT] [Task 008] Updated styles with inline styling and CSS styles.

--- Apps/01-first_app/src/index.js
+++ Apps/01-first_app/src/index.js
@@ -5,16 +5,35 @@ const number = 123;
 const developer_gh_name = "@ArielMAJ";
 const current_year = new Date().getFullYear();
 const random_img_link = "https://picsum.photos/200";
+const custom_li_style = { color: "blue", width: "5em", padding: ".2em", border: "2px dashed lightblue", borderRadius: "10px" };
+
+const current_hour = new Date().getHours();
+let message = {};
+
+if (current_hour >= 18 || current_hour <= 4) {
+  message.text = "Good evening";
+  message.style = { color: "blue", borderBottomColor: "blue" };
+} else if (current_hour >= 12) {
+  message.text = "Good afternoon";
+  message.style = { color: "green", borderBottomColor: "green" };
+} else {
+  message.text = "Good morning";
+  message.style = { color: "red", borderBottomColor: "red" };
+}
 
 ReactDOM.render(
   <div>
-    <h1>Lorem ipsum</h1>
+    <h1 className="heading" style={message.style}>
+      {message.text}
+    </h1>
+
+    <h2>Lorem ipsum</h2>
     <ul>
       <li>dolor</li>
       <li>sit</li>
       <li>amet</li>
       <li>Constant: {number}</li>
-      <li>Random: {Math.floor(Math.random() * 100)}</li>
+      <li style={custom_li_style}>Random: {Math.floor(Math.random() * 100)}</li>
       <li>Curly: {"{{}}"}</li>
     </ul>
 
@@ -65,7 +84,7 @@ ReactDOM.render(
     </div>
 
     <hr />
-    <p className="heading">Created by {developer_gh_name}</p>
+    <p className="heading2">Created by {developer_gh_name}</p>
     <p>Copyright {current_year}</p>
   </div>,
   document.getElementById("root")
